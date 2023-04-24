e745cc928cafdbbeb918739e412dc55ee85325db
MSKim0215 <rlaalstjqu05@gmail.com>
2023-04-24 14:33:09 +0900

    feat : add to Managers
    1) 모든 매니저 관리하는 Managers 구현

---
+++ Assets/Scripts/Managers/Managers.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    private static Managers s_instance;  
+
+    public static Managers Instance
+    {
+        get
+        {
+            Init();
+            return s_instance;   
+        }
+    }
+
+    private void Start()
+    {
+        Init();
+    }
+
+    private static void Init()
+    {
+        if(s_instance == null)
+        {
+            GameObject target = GameObject.Find("@Managers");
+            if(target == null)
+            {
+                target = new GameObject { name = "@Managers" };
+                target.AddComponent<Managers>();
+            }
+
+            DontDestroyOnLoad(target);
+            s_instance = target.GetComponent<Managers>();
+        }
+    }
+}
